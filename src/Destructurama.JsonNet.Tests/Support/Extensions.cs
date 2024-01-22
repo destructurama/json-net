901a5bc028e7c58cc2b1675dd787727df9966337
Ivan Maximov <sungam3r@yandex.ru>
2024-01-22 08:36:24 +0300

    1

--- src/Destructurama.JsonNet.Tests/Support/Extensions.cs
+++ src/Destructurama.JsonNet.Tests/Support/Extensions.cs
@@ -16,10 +16,8 @@ using Serilog.Events;
 
 namespace Destructurama.JsonNet.Tests.Support;
 
-public static class Extensions
+internal static class Extensions
 {
     public static object? LiteralValue(this LogEventPropertyValue @this)
-    {
-        return ((ScalarValue)@this).Value;
-    }
+        => ((ScalarValue)@this).Value;
 }
