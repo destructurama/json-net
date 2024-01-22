5db2e25d12bab4e4532d47964482edc76c199825
github-action-benchmark <github@users.noreply.github.com>
2024-01-22 06:10:10 +0000

    add Benchmarks (benchmarkdotnet) benchmark result for b3950efe18cd156070e164a0bf79ea7bbc28ff1e

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1705903810505,
+  "repoUrl": "https://github.com/destructurama/json-net",
+  "entries": {
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "sungam3r@yandex.ru",
+            "name": "Ivan Maximov",
+            "username": "sungam3r"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b3950efe18cd156070e164a0bf79ea7bbc28ff1e",
+          "message": "Add benchmark (#28)",
+          "timestamp": "2024-01-22T09:09:00+03:00",
+          "tree_id": "8838567b028be8eec3a56ecfe4979b687f535774",
+          "url": "https://github.com/destructurama/json-net/commit/b3950efe18cd156070e164a0bf79ea7bbc28ff1e"
+        },
+        "date": 1705903810097,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.JsonNetBenchmarks.Destructure",
+            "value": 2035.4098457923303,
+            "unit": "ns",
+            "range": "± 7.513269390553504"
+          }
+        ]
+      }
+    ]
+  }
+}
