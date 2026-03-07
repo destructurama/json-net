0935a65fd6b0a635eaafa9157d8110279f65ee60
github-action-benchmark <github@users.noreply.github.com>
2026-03-07 11:49:15 +0000

    add Benchmarks (benchmarkdotnet) benchmark result for 098627537656c16c29db10c8992529a0d8cddcdf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1770143894452,
+  "lastUpdate": 1772884155450,
   "repoUrl": "https://github.com/destructurama/json-net",
   "entries": {
     "Benchmarks": [
@@ -2520,6 +2520,36 @@ window.BENCHMARK_DATA = {
             "value": 1760.5195125852313,
             "unit": "ns",
             "range": "± 4.858640856813813"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "098627537656c16c29db10c8992529a0d8cddcdf",
+          "message": "Bump actions/upload-artifact from 6 to 7 (#137)\n\n* Bump actions/upload-artifact from 6 to 7\n\nBumps [actions/upload-artifact](https://github.com/actions/upload-artifact) from 6 to 7.\n- [Release notes](https://github.com/actions/upload-artifact/releases)\n- [Commits](https://github.com/actions/upload-artifact/compare/v6...v7)\n\n---\nupdated-dependencies:\n- dependency-name: actions/upload-artifact\n  dependency-version: '7'\n  dependency-type: direct:production\n  update-type: version-update:semver-major\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\n\n* fix\n\n---------\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>\nCo-authored-by: Ivan Maximov <sungam3r@yandex.ru>",
+          "timestamp": "2026-03-07T14:48:13+03:00",
+          "tree_id": "5a88c234379f7f51127ef8ad5723df53aad20b21",
+          "url": "https://github.com/destructurama/json-net/commit/098627537656c16c29db10c8992529a0d8cddcdf"
+        },
+        "date": 1772884154654,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.JsonNetBenchmarks.Destructure",
+            "value": 1920.1408142089845,
+            "unit": "ns",
+            "range": "± 15.878565807277717"
           }
         ]
       }
